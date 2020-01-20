f5054cb7ee57461b1480608d31c0ec4ae8978ea1
sjw000324 <sjw000324@gmail.com>
2020-01-20 22:13:16 +0900

    movement of block by reading keyboard input  (+Spacebar, Left, Right, Down except UP key worked)

--- Tetris/main.cpp
+++ Tetris/main.cpp
@@ -1,17 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <conio.h>
 #include <Windows.h>
 
 #define WIDTH 12					// 맵의 너비
 #define HEIGHT 27					// 맵의 높이
 #define BLOCK_SIZE 4				// 블록 하나에 포함되는 작은 블록의 갯수(모두 4개)
 #define FRAME_PER_SEC 240		// 테트리스의 fps. 초당 240회의 연산을 하도록 고정 (고정 안할 시 초당 n천회 이상)
+#define ARROW_KEY_DEFAULT 224		// 방향키를 입력받을 때, 방향키의 ASCII값에 앞서 입력되는 ASCII값
+#define KEY_LEFT 75
+#define KEY_RIGHT 77
+#define KEY_UP 72
+#define KEY_DOWN 80
+#define KEY_SPACE 32
 
 #define GET_MIN(n1, n2) ((n1) < (n2) ? (n1) : (n2))		// 더 작은 수를 리턴하는 매크로 함수
 
 typedef enum type { I, O, Z, S, J, L, T } TYPE; // 블럭의 종류(7가지)를 열거형으로 정의.
-typedef enum direction { LEFT, RIGHT, DOWN } DIRECTION;
+typedef enum direction { DIR_LEFT, DIR_RIGHT, DIR_DOWN } DIRECTION;
 
 //typedef struct point {
 //	int x, y;
@@ -21,7 +28,7 @@ typedef struct block {
 	TYPE blockType;
 	POINT blockPoint[4]; // 배열의 첫 요소가 기준점.
 	int rotation, rotationCycle;	// 회전 횟수, 반복 주기.
-	int nFrame = FRAME_PER_SEC; // 떨어지는 속도.
+	short nFrame = 0;	// 떨어지는 속도.
 } BLOCK;
 
 void setMap(int map[][WIDTH]);
@@ -32,26 +39,87 @@ void removeBlock(const int map[][WIDTH], BLOCK* p);
 void moveBlock(const int map[][WIDTH], BLOCK* p, char direction);
 void rotateBlock(const int map[][WIDTH], BLOCK* p);
 void putBlock(const int map[][WIDTH], BLOCK* p);
-void showBlockPrev(const int map[][WIDTH], BLOCK* pBlock);
+void putBlockPrev(const int map[][WIDTH], BLOCK* pBlock);
+void removeBlockPrev(const int map[][WIDTH], BLOCK* pBlock);
 void gotoxy(int x, int y);
+int getDeltaY(const int map[][WIDTH], BLOCK* pBlock);
 
 int main() {
 	srand((unsigned int)time(NULL));
 	system("mode con cols=80 lines=30");		// 프롬프트 창 크기 조절
 
+	int deltaY = 0;
 	int map[HEIGHT][WIDTH] = { 0 };
 	setMap(map);
 	printMap(map);
 
 	BLOCK a;
 	setBlock(&a);
-	putBlock(map, &a);
 	gotoxy(5, 27);
 	printf("a.nFrame = ");
 
 	while (a.blockPoint[0].y < HEIGHT - 2) {		// 블록이 맵 바닥까지 떨어질 때까지 반복
 		gotoxy(16, 27);
 		printf("%03d", a.nFrame);
+
+
+		/* 키보드 입력받는 부분 */
+		if (_kbhit()) {
+			switch (_getch()) {
+				case KEY_SPACE :
+					deltaY = getDeltaY(map, &a);
+					removeBlock(map, &a);
+					for (int i = 0; i < BLOCK_SIZE; i++) {
+						a.blockPoint[i].y += deltaY;
+					}
+					putBlock(map, &a);
+					break;
+				case ARROW_KEY_DEFAULT :
+					switch (_getch()) {
+						case KEY_DOWN:
+							removeBlock(map, &a);
+							for (int i = 0; i < BLOCK_SIZE; i++) {
+								a.blockPoint[i].y++;
+								a.nFrame = FRAME_PER_SEC;
+							}
+							putBlock(map, &a);
+							break;
+						case KEY_LEFT :
+							removeBlock(map, &a);
+							for (int i = 0; i < BLOCK_SIZE; i++) {
+								if (a.blockPoint[i].x - 1 <= 0)
+									break;
+								else if (i == BLOCK_SIZE - 1) {
+									for (int i = 0; i < BLOCK_SIZE; i++) {
+										a.blockPoint[i].x--;
+									}
+								}
+							}
+							putBlock(map, &a);
+							break;
+						case KEY_RIGHT : 
+							removeBlock(map, &a);
+							for (int i = 0; i < BLOCK_SIZE; i++) {
+								if (WIDTH - 1 <= a.blockPoint[i].x + 1)
+									break;
+								else if (i == BLOCK_SIZE - 1) {
+									for (int i = 0; i < BLOCK_SIZE; i++) {
+										a.blockPoint[i].x++;
+									}
+								}
+							}
+							putBlock(map, &a);
+							break;
+						case KEY_UP:
+							removeBlock(map, &a);
+							rotateBlock(map, &a);
+							putBlock(map, &a);
+							break;
+					}
+			}
+		}
+		/* 키보드 입력받는 부분 끝*/
+
 
 		if (a.nFrame <= 0) {
 			moveBlock(map, &a, 'a');
@@ -92,7 +160,7 @@ void printMap(const int map[][WIDTH]) {
 
 void setBlock(BLOCK* p) {		// 블럭 의 속성값 초기화.
 	POINT* point = p->blockPoint;
-	setPoint(point, 6, 0);	// 블럭의 기준점을 (6, 2)으로 초기화
+	setPoint(point, 2 * 3, -1);	// 블럭의 기준점을 (2 * 3, -1)으로 초기화
 
 	//p->blockType = (TYPE)(rand() % 7);
 	p->blockType = O;		// 임시로 O형 블록만 선택함.		아래 switch문 완성시 윗 라인의 주석 코드로 교체
@@ -130,6 +198,7 @@ void setPoint(POINT* p, int x, int y) {
 }
 
 void removeBlock(const int map[][WIDTH], BLOCK* p) { // 이 블럭에 공백을 덮어씌워 지운다.
+	removeBlockPrev(map, p);
 	for (int i = 0; i < BLOCK_SIZE; i++) {
 		if ((0 < p->blockPoint[i].x && p->blockPoint[i].x < WIDTH - 1) && (0 < p->blockPoint[i].y && p->blockPoint[i].y < HEIGHT - 1)) {   // map의 테두리가 아닐 경우에만
 			gotoxy(2 * p->blockPoint[i].x, p->blockPoint[i].y);
@@ -159,14 +228,37 @@ void rotateBlock(const int map[][WIDTH], BLOCK* p) { // 기준점 중심으로 반시계 �
 }
 
 void putBlock(const int map[][WIDTH], BLOCK* p) { // moveBlock에 의해 옮겨진 좌표로 블럭을 출력한다.
+	putBlockPrev(map, p);
 	for (int i = 0; i < BLOCK_SIZE; i++) {
 		gotoxy(2 * p->blockPoint[i].x, p->blockPoint[i].y);
 		printf("■");
 	}
-	showBlockPrev(map, p);
 }
 
-void showBlockPrev(const int map[][WIDTH], BLOCK* pBlock) {		// 떨어지는 블록이 떨어질 위치를 미리 보여줌
+void putBlockPrev(const int map[][WIDTH], BLOCK* pBlock) {		// 떨어지는 블록이 떨어질 위치를 미리 보여줌
+	int deltaY = getDeltaY(map, pBlock);
+
+	for (int i = 0; i < BLOCK_SIZE; i++) {
+		gotoxy(2 * pBlock->blockPoint[i].x, pBlock->blockPoint[i].y + deltaY);
+		printf("□");
+	}
+}
+
+void removeBlockPrev(const int map[][WIDTH], BLOCK* pBlock) {
+	int deltaY = getDeltaY(map, pBlock);
+
+	for (int i = 0; i < BLOCK_SIZE; i++) {
+		gotoxy(2 * pBlock->blockPoint[i].x, pBlock->blockPoint[i].y + deltaY);
+		printf("  ");
+	}
+}
+
+void gotoxy(int x, int y) {		// 커서를 해당 좌표로 이동
+	COORD Cur = { x, y };
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), Cur);
+}
+
+int getDeltaY(const int map[][WIDTH], BLOCK* pBlock) {	// 떨어지는 블럭과 바닥 간의 거리를 리턴하는 함수
 	int deltaY = HEIGHT;		// 떨어지는 블럭과 바닥간의 거리를 저장. 최종적으로 최솟값을 얻는 것이 목표이므로 최댓값으로 초기화
 
 	for (int i = 0; i < BLOCK_SIZE; i++) {
@@ -178,13 +270,5 @@ void showBlockPrev(const int map[][WIDTH], BLOCK* pBlock) {		// 떨어지는 블록이 
 		}
 	}
 
-	for (int i = 0; i < BLOCK_SIZE; i++) {
-		gotoxy(2 * pBlock->blockPoint[i].x, pBlock->blockPoint[i].y + deltaY);
-		printf("□");
-	}
-}
-
-void gotoxy(int x, int y) {		// 커서를 해당 좌표로 이동
-	COORD Cur = { x, y };
-	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), Cur);
+	return deltaY;
 }
